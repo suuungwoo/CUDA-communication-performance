f9f3aaa7707b73bda3e6e1efe387b8ce532040e7
Sungwoo <suuungwoo@gmail.com>
2019-08-02 19:03:00 +0900

    データサイズを動的に

--- vector_addition.cu
+++ vector_addition.cu
@@ -1,8 +1,5 @@
-#include <cuda.h>
 #include <stdio.h>
-// #include <cuda_rentime.h>
-
-#define N 1024
+#include <stdlib.h>
 
 __global__ void VecAdd(float *A, float *B, float *C) {
   int i = threadIdx.x;
@@ -10,26 +7,56 @@ __global__ void VecAdd(float *A, float *B, float *C) {
 }
 
 int main() {
-  float h_A[N], h_B[N], h_C[N];
-  size_t size = N * sizeof(float);
-  float *d_A;
-  cudaMalloc((void **)&d_A, size);
-  float *d_B;
-  cudaMalloc((void **)&d_B, size);
-  float *d_C;
-  cudaMalloc((void **)&d_C, size);
+  long int N;
+  FILE *outputfile;
+  outputfile = fopen("sync.csv", "w"); // ファイルを書き込み用にオープン(開く)
+  if (outputfile == NULL) {  // オープンに失敗した場合
+    printf("cannot open\n"); // エラーメッセージを出して
+    exit(1);                 // 異常終了
+  }
 
-  cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+  for (N = 2; N <= 1024000; N *= 2) {
+    float h_A[N], h_B[N], h_C[N];
+    size_t size = N * sizeof(float);
+    int i;
 
-  int threadsPerBlock = N;
-  int threadsPerGrid = 1;
+    time_t start_download, stop_download, start_kernel, stop_kernel;
 
-  VecAdd<<<threadsPerGrid, threadsPerBlock>>>(d_A, d_B, d_C);
+    float *d_A;
+    cudaMalloc((void **)&d_A, size);
+    float *d_B;
+    cudaMalloc((void **)&d_B, size);
+    float *d_C;
+    cudaMalloc((void **)&d_C, size);
 
-  cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    srand((unsigned)time(NULL));
+    for (i = 0; i < N; i++) {
+      h_A[i] = rand() % 10;
+      h_B[i] = rand() % 10;
+    }
 
-  cudaFree(d_A);
-  cudaFree(d_B);
-  cudaFree(d_C);
+    start_download = clock();
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    stop_download = clock();
+
+    int threadsPerBlock = N;
+    int threadsPerGrid = 1;
+
+    start_kernel = clock();
+    VecAdd<<<threadsPerGrid, threadsPerBlock>>>(d_A, d_B, d_C);
+    stop_kernel = clock();
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    fprintf(outputfile, "%ld,%f,%f\n", N * 8,
+            (double)(stop_download - start_download),
+            (double)(stop_kernel - start_kernel));
+  }
+  fclose(outputfile);
+  return 0;
 }
