df8f6a02eee99682c91a82c4f4fb81351c7c5d1b
Sungwoo <suuungwoo@gmail.com>
2019-08-02 19:04:15 +0900

    非同期通信版を追加

---
+++ vector_addition_async.cu
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void VecAdd(float *A, float *B, float *C) {
+  int i = threadIdx.x;
+  C[i] = A[i] + B[i];
+}
+
+int main() {
+  long int N;
+  FILE *outputfile;
+  outputfile = fopen("async.csv", "w"); // ファイルを書き込み用にオープン(開く)
+  if (outputfile == NULL) {  // オープンに失敗した場合
+    printf("cannot open\n"); // エラーメッセージを出して
+    exit(1);                 // 異常終了
+  }
+
+  for (N = 2; N <= 1024000; N *= 2) {
+    float h_A[N], h_B[N], h_C[N];
+    size_t size = N * sizeof(float);
+    int i;
+
+    time_t start_download, stop_download, start_kernel, stop_kernel;
+
+    float *d_A;
+    cudaMalloc((void **)&d_A, size);
+    float *d_B;
+    cudaMalloc((void **)&d_B, size);
+    float *d_C;
+    cudaMalloc((void **)&d_C, size);
+
+    srand((unsigned)time(NULL));
+    for (i = 0; i < N; i++) {
+      h_A[i] = rand() % 10;
+      h_B[i] = rand() % 10;
+    }
+
+    start_download = clock();
+    cudaMemcpyAsync(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpyAsync(d_B, h_B, size, cudaMemcpyHostToDevice);
+    stop_download = clock();
+
+    int threadsPerBlock = N;
+    int threadsPerGrid = 1;
+
+    start_kernel = clock();
+    VecAdd<<<threadsPerGrid, threadsPerBlock>>>(d_A, d_B, d_C);
+    stop_kernel = clock();
+
+    cudaMemcpyAsync(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    fprintf(outputfile, "%ld,%f,%f\n", N * 8,
+            (double)(stop_download - start_download),
+            (double)(stop_kernel - start_kernel));
+  }
+  fclose(outputfile);
+  return 0;
+}
